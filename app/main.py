a12668ecb67bf3e3828461e7075cf98211e4c85d
Diego-DPL <info@diegodpl.com>
2024-10-04 09:25:48 +0200

    Heroku

--- app/main.py
+++ app/main.py
@@ -1,16 +1,35 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from utils.transcription import Transcriber
 import asyncio
-
-from dotenv import load_dotenv
-load_dotenv()
 import os
-print("GOOGLE_APPLICATION_CREDENTIALS:", os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+import json
+from google.oauth2 import service_account
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Cargar las credenciales de Google Cloud desde una variable de entorno
+credentials_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+if credentials_json:
+    # Convertir el string JSON a un objeto de credenciales
+    credentials_info = json.loads(credentials_json)
+    credentials = service_account.Credentials.from_service_account_info(credentials_info)
+else:
+    raise ValueError("No se encontró la variable de entorno GOOGLE_APPLICATION_CREDENTIALS")
+
+# Cargar las credenciales de Google Cloud desde un archivo .env
+#from dotenv import load_dotenv
+#load_dotenv()
+#import os
+#print("GOOGLE_APPLICATION_CREDENTIALS:", os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+#eliminar credentials=credentials de la función websocket_endpoint
+
 
 app = FastAPI()
 
 @app.websocket("/ws/audio")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket, credentials=credentials):
     await websocket.accept()
     message_queue = asyncio.Queue()
     transcriber = Transcriber(message_queue)
